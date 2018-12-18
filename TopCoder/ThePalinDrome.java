47ab88a60ee400606be4a8c646c1757f07981ccd
SeongYeob Jeong <ubi.toffee@gmail.com>
2018-12-18 14:10:25 +0900

    Update ThePalinDrome.java

--- TopCoder/ThePalinDrome.java
+++ TopCoder/ThePalinDrome.java
@@ -1,28 +1,14 @@
 public class ThePalindrome {
     public int find(String s) {
-        int size = s.length();
-        String reverse = new StringBuilder(s).reverse().toString();
-       	for (int i = 0; i < reverse.length(); i++) {
-            String palindrome = s + reverse.substring(reverse.length() - i, reverse.length());
-            String half = null;
-            String lastHalf = null;
-            
-            if (palindrome.length() % 2 == 0) {
-                half = palindrome.substring(0, (int)(palindrome.length() / 2));
-                lastHalf = palindrome.substring((int)(palindrome.length() / 2), palindrome.length());
-                lastHalf = new StringBuilder(lastHalf).reverse().toString();
-            } else {
-                half = palindrome.substring(0, (int)(palindrome.length() / 2));
-                lastHalf = palindrome.substring((int)(palindrome.length() / 2) + 2, palindrome.length());
-                lastHalf = new StringBuilder(lastHalf).reverse().toString();
-            }
-                
-            if (half.equals(lastHalf)) {
-                size = palindrome.length();
+       	for (int i = 0; i < s.length(); i++) {
+            if (i >= s.length() - i - 1) {
                 break;
+            }
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
+                s += s.charAt(i);
             }
         }
         
-        return size;
+        return s.length();
     }
 }
