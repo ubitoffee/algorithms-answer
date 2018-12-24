9eecf021c52e8f2e7e3b326342714cac8ac7e252
SeongYeob Jeong <ubi.toffee@gmail.com>
2018-12-24 15:02:41 +0900

    Update CrazyBot.java

--- TopCoder/CrazyBot.java
+++ TopCoder/CrazyBot.java
@@ -1,30 +1,29 @@
 public class CrazyBot {
-    double result = 0;
+    boolean[][] grid = new boolean[100][100];
     double[] percentage = new double[4];
-    int cnt = 0;
+    int[] vx = new int[]{1, -1, 0, 0};
+    int[] vy = new int[]{0, 0, -1, 1};
+    
     int row = 0;
     int col = 0;
     
     public double getProbability(int n, int east, int west, int south, int north) {
         percentage = new double[]{ east/100, west/100, south/100, north/100 };
-        cnt = n;
-        for (int i = 0; i < percentage.length; i++) {
-            recursive(0, 1, percentage[i]);
-        }
+        
+        double result = recursive(50, 50, n);
         
         return result;
     }
     
-    private void recursive(int j, double finalPercent, double pcnt) {
-        if (j == cnt) {
-            result += finalPercent;
-        } else if (finalPercent == 0) {
-        } else {
-            j++;
-            for (int i = 0; i < percentage.length; i++) {
-                finalPercent *= pcnt;
-            	recursive(j, finalPercent, percentage[i]);
-            }
+    private double recursive(int x, int y, int n) {
+        if (grid[x][y]) return 0;
+       	if (n == 0) return 1;
+        
+        double ret = 0;        
+        for (int i = 0; i < percentage.length; i++) {
+            ret = recursive(x + vx[i], y + vy[i], n - 1) * percentage[i];
         }
+        
+        return ret;
     }
 }
